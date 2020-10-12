5d9b1dfff7e35478be9d820862fc3fce837eabdd
Rongzhi Wang <aidenwang0826@gmail.com>
2020-10-12 14:45:14 +1100

    add zodiac on profile

--- src/config/url.js
+++ src/config/url.js
@@ -1,3 +1,4 @@
 export const authUrl = "/api/v1/user/login";
 export const signOutUrl = "/api/v1/user/logout";
 export const registUrl = "/api/v1/user/register";
+export const zodiacUrl = "/api/v1/user/chinese-zodiac";
--- src/views/MyProfile/MyProfile.js
+++ src/views/MyProfile/MyProfile.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Typography from '../../components/Typography/Typography';
+import React, { useState, useEffect, useCallback } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Typography from "../../components/Typography/Typography";
+import request from "../../utils/request";
+import { zodiacUrl } from "../../config/url";
+import Article from "../Article/Article";
 
 const styles = (theme) => ({
   root: {
@@ -10,32 +13,65 @@ const styles = (theme) => ({
     marginBottom: theme.spacing(4),
   },
   images: {
-    display: 'grid',
-    gridTemplateColumns: '640px 640px',
-    gridGap: '10px'
-  }
+    display: "grid",
+    gridTemplateColumns: "640px 640px",
+    gridGap: "10px",
+  },
+  zodiacBody: {
+    textAlign: "center",
+    fontSize: 30,
+  },
+  point: {
+    cursor: "pointer",
+  },
 });
 
-
-function MyProfile(props) {
+const MyProfile = (props) => {
   const { classes } = props;
+  const [result, setResult] = useState([]);
+  const [selected, setSelected] = useState(-1);
+  const handleClick = useCallback((id) => setSelected(id), []);
 
-  
+  useEffect(() => {
+    const fetchZodiac = async () => {
+      let url = "http://localhost:8088" + zodiacUrl;
+      fetch(url)
+        .then((response) => response.json())
+        .then((res) => {
+          console.log(res);
+          setResult(res);
+        });
+    };
+    fetchZodiac();
+    console.log(result);
+  }, []);
+
+  let list = null;
+  if (result) {
+    list = result.map((article) => (
+      <Article
+        key={article.id}
+        {...article}
+        isSelected={article.id === selected}
+        onClick={handleClick}
+        classes={classes.point}
+      />
+    ));
+  }
 
   return (
     <Container className={classes.root} component="section">
       <Typography variant="h4" marked="center" align="center" component="h2">
-        My Portfolio 
+        My Portfolio
       </Typography>
-      <div className={classes.images}>
-         My Profile
-      </div>
+      <div className={classes.images}>My Profile</div>
+      <div className={classes.zodiacBody}>{list}</div>
     </Container>
   );
-}
+};
 
 MyProfile.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(MyProfile);
+export default React.memo(withStyles(styles)(MyProfile));
